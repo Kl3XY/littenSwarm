e5bd951b2e024e2f47a79067b9f1e12dcccc98d9
Martin Gaffke <mga@spi.de>
2025-05-15 15:18:16 +0200

    karma karma karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,51 +1,18 @@
-// karma.conf.js
 module.exports = function(config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma'),
-    ],
-
-    client: {
-      jasmine: {
-        // you can add config options here
-      },
-      clearContext: false, // leave Jasmine Spec Runner output visible in browser
-    },
-
-    jasmineHtmlReporter: {
-      suppressAll: true, // removes duplicated traces
-    },
-
-    coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/littenSwarm'),
-      subdir: '.',
-      reporters: [{ type: 'html' }, { type: 'text-summary' }],
-    },
-
-    reporters: ['progress', 'kjhtml'],
-
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
+    // ... keep your existing config here ...
 
     browsers: ['ChromeHeadlessNoSandbox'],
 
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-setuid-sandbox'],
-      },
+        flags: ['--no-sandbox', '--disable-setuid-sandbox']
+      }
     },
 
-    singleRun: false,
-    restartOnFileChange: true,
+    singleRun: true,  // optional, good for CI
+
+    // other configs ...
   });
 };
